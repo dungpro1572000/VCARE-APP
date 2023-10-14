f89ccc7c5363c620aea805f124826eaa59fdfc7b
dungpro1572000 <dungnt1572000@gmail.com>
2023-10-14 10:31:57 +0700

    check

--- app/src/main/java/com/example/vcare_app/personal/PersonalFragment.kt
+++ app/src/main/java/com/example/vcare_app/personal/PersonalFragment.kt
@@ -1,6 +1,7 @@
 package com.example.vcare_app.personal
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,6 +36,7 @@ class PersonalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.d("Cai deo j","hehe")
         return inflater.inflate(R.layout.fragment_personal, container, false)
     }
 
